4138288a6079c0bd9eff5b7e71206704863a7199
Gabriele Petroni <gabriele.petroni2k@gmail.com>
2022-03-31 16:39:15 +0200

    Commit certificati +

--- BYODclient.py
+++ BYODclient.py
@@ -1,58 +1,58 @@
-# clientsocket.py
-import json
 import socket
-import time
-import datetime
 import ssl
+import os
+import time
 
-HOST = "127.0.0.1"
-# HOST = "25.35.53.183"  # SERVER: Gabriele - Hamachi IPV4
-PORT = 3030  # The port used by the server
+
+
+# IP address and the port number of the server
+sslServerIP = "127.0.0.1";
+sslServerPort = 15001;
 
 # Create an SSL context
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.verify_mode = ssl.CERT_REQUIRED
+context = ssl.SSLContext();
+context.verify_mode = ssl.CERT_REQUIRED;
 
 # Load CA certificate with which the client will validate the server certificate
-context.load_verify_locations("./CA.pem")
+context.load_verify_locations("./certca/ca.pem");
 
 # Load client certificate
-context.load_cert_chain("./certif.crt", "./keys.key")
+context.load_cert_chain(certfile="./certclient/client1.crt", keyfile="./certclient/client1.key");
 
 # Create a client socket
-clientSocket = socket.socket()
+clientSocket = socket.socket();
 
 # Make the client socket suitable for secure communication
-secureClientSocket = context.wrap_socket(clientSocket)
-secureClientSocket.connect((HOST, PORT))
+secureClientSocket = context.wrap_socket(clientSocket);
+secureClientSocket.connect((sslServerIP, sslServerPort));
 
 # Obtain the certificate from the server
-server_cert = secureClientSocket.getpeercert()
+server_cert = secureClientSocket.getpeercert();
 
 # Validate whether the Certificate is indeed issued to the server
-subject = dict(item[0] for item in server_cert['subject'])
-commonName = subject['commonName']
+subject = dict(item[0] for item in server_cert['subject']);
+commonName = subject['commonName'];
 
 if not server_cert:
-    raise Exception("Unable to retrieve server certificate")
+    raise Exception("Unable to retrieve server certificate");
 
 if commonName != 'DemoSvr':
-    raise Exception("Incorrect common name in server certificate")
+    raise Exception("Incorrect common name in server certificate");
 
-notAfterTimestamp = ssl.cert_time_to_seconds(server_cert['notAfter'])
-notBeforeTimestamp = ssl.cert_time_to_seconds(server_cert['notBefore'])
-currentTimeStamp = time.time()
+notAfterTimestamp = ssl.cert_time_to_seconds(server_cert['notAfter']);
+notBeforeTimestamp = ssl.cert_time_to_seconds(server_cert['notBefore']);
+currentTimeStamp = time.time();
 
 if currentTimeStamp > notAfterTimestamp:
-    raise Exception("Expired server certificate")
+    raise Exception("Expired server certificate");
 
 if currentTimeStamp < notBeforeTimestamp:
-    raise Exception("Server certificate not yet active")
+    raise Exception("Server certificate not yet active");
 
 # Safe to proceed with the communication
-msgReceived = secureClientSocket.recv(1024)
-print("Secure communication received from server:%s" % msgReceived.decode())
+msgReceived = secureClientSocket.recv(1024);
+print("Secure communication received from server:%s" % msgReceived.decode());
 
 # Close the sockets
-secureClientSocket.close()
-clientSocket.close()
+secureClientSocket.close();
+clientSocket.close();
