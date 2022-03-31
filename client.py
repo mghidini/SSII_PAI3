1ecbc51b6b77ecd0d44a0fabe1dd43c806b83c6c
mghidini <92437378+mghidini@users.noreply.github.com>
2022-03-31 17:54:18 +0200

    client-server

--- client.py
+++ client.py
@@ -6,9 +6,9 @@ ssl_sock = ssl.wrap_socket(s,
 			   ca_certs = "server.crt",
 			   cert_reqs = ssl.CERT_REQUIRED,
 			   ciphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:AES128-GCM-SHA256:AES128-256:HIGH:")
-ssl_socket.connect(('localhost', 10035))
+ssl_sock.connect(('localhost', 10035))
 
 while True:
-	mensaje = raw_input("Mensaje a enviar:")
+	mensaje = input("Mensaje a enviar:")
 	ssl_sock.write(mensaje)
 	ssl_sock.close()
