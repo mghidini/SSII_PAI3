ab2fb95435e5b0298865cadc878908a4f69b347f
Gabriele Petroni <gabriele.petroni2k@gmail.com>
2022-04-01 15:29:46 +0200

    Aggiunta database e modifiche server

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-import socket, ssl
+import socket, ssl, json
 
 bindsocket = socket.socket()
-bindsocket.bind(('localhost', 10035))
+bindsocket.bind(('25.35.53.183', 10035))
 bindsocket.listen(5)
 
 while True:
@@ -11,6 +11,9 @@ while True:
                                  certfile="server.crt",
                                  keyfile="server.key",
                                  ciphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:AES128-GCM-SHA256:AES128-SHA256:HIGH:")
-    print("Recibido del cliente: ", connstream.read())
-    connstream.shutdown(socket.SHUT_RDWR)
-    connstream.close()
+    data = connstream.recv(1024)
+    message = json.loads(data)
+    print("Recibido del cliente: ", message)
+    connstream.sendall(b"Ricevuto!")
+connstream.shutdown(socket.SHUT_RDWR)
+connstream.close()
