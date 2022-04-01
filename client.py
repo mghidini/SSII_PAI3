84a8ae4b576ad545efdbf544bc8de0b1d5929c1e
mghidini <92437378+mghidini@users.noreply.github.com>
2022-04-01 15:26:11 +0200

    client giusto

--- client.py
+++ client.py
@@ -1,14 +1,34 @@
-import socket,ssl
+import socket
+import ssl
+import json
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# HOST = "127.0.0.1"
+HOST = "25.35.53.183"  # SERVER: Gabriele - Hamachi IPV4
+PORT = 10035  # The port used by the server
+
+username = input("Insert username:")  # str type
+password = input("Insert password:")  # str type
+
+data = {
+    "username": username,
+    "password": password}
+
+serialized = json.dumps(data)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ssl_sock = ssl.wrap_socket(s,	
-			   ca_certs = "server.crt",
-			   cert_reqs = ssl.CERT_REQUIRED,
-			   ciphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:AES128-GCM-SHA256:AES128-256:HIGH:")
-ssl_sock.connect(('localhost', 10035))
+ssl_sock = ssl.wrap_socket(s,
+                           ca_certs="server.crt",
+                           cert_reqs=ssl.CERT_REQUIRED,
+                           ciphers="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:AES128-GCM-SHA256:AES128-256:HIGH:")
+ssl_sock.connect((HOST, PORT))
 
-while True:
-	mensaje = input("Mensaje a enviar:")
-	ssl_sock.write(mensaje)
-	ssl_sock.close()
+
+ssl_sock.write((bytes(serialized, "utf-8")))
+data = ssl_sock.recv(1024)
+print(str(data))
+#while True:
+ssl_sock.close()
